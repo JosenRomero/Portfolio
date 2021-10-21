88a93504f68863a8720e59641a16fd7f4f0cbe98
jose manuel <romerojosemanuel@hotmail.com>
2021-10-20 19:25:05 -0600

    initial commit - webpack config

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    entry: {
+		main: './src/index.js'
+	},
+
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].[contenthash].js'
+	},
+
+    // mode: 'development',
+
+	// devtool: 'source-map',
+
+	devServer: {
+		open: true,
+		port: 3001
+	},
+
+	module: {
+
+		rules: [
+			{
+				test: /\.js$/,
+        		use: {
+          			loader: 'babel-loader',
+          			options: { 
+						presets: [
+							['@babel/preset-env']
+						] 
+					}
+        		}
+			},
+			{
+				test: /\.css$/, 
+				use: [
+					'style-loader', 
+					'css-loader'
+				]	
+			}
+		]
+
+	},
+
+	plugins: [
+
+		new CleanWebpackPlugin({
+			root: path.resolve(__dirname, "./build")
+		}),
+
+		new HtmlWebpackPlugin({ 
+			template: './src/index.html',
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkAttributes: true,
+				useShortDoctype: true
+			}
+		})
+
+	]
+
+}
