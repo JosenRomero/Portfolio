5397c48e0fccb40b2d61344c16870859602a3502
jose manuel <romerojosemanuel@hotmail.com>
2021-10-21 18:53:48 -0600

    added animate.css and added header, sections and footer in index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hi");
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'animate.css';
+import './main.css';
