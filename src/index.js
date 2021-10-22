e8e41ef230810c2cab9273dac2ede36fbb5c3be0
jose manuel <romerojosemanuel@hotmail.com>
2021-10-22 17:31:00 -0600

    changed animate.css for aos Animate On Scroll Library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'animate.css';
 import './main.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init(); // AOS - Animate on scroll library
