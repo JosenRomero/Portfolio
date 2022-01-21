e44c09ed69b5e296714b61d6c38613e05cd22e3f
Josen Romero <romerojosemanuel@hotmail.com>
2022-01-20 19:35:26 -0600

    removed bootstrap-icons and devicon, instead added some svg

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'devicon/devicon.min.css'
 import './css/main.css';
 
 import AOS from 'aos';
